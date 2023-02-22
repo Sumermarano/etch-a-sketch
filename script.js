bc4fbf72991d1295eb762968f758f41f78e88bb2
sumermarano <sumermarano@gmail.com>
2023-02-22 23:26:48 +0200

    fix grid size when changing dimensions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const grid = document.querySelector('.grid');
 
 function getGridSize() {
     const size = prompt("Grid Size x*x ? (x < 100)");
-    if (size === null) return 64;
+    if (size === null) return 8;
     if (size > 100 || size < 1) getGridSize();
     return +size;
 }
@@ -11,14 +11,13 @@ function showGrid(size) {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
-    for (let i = 0; i < size; i++) {
-        const row = document.createElement('div');
-        for (let j = 0; j < size; j++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            row.appendChild(square);
-        }
-        grid.appendChild(row);
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${500/size}px`;
+        square.style.height = `${500/size}px`;
+        square.style.backgroundColor = '#F16767';
+        grid.appendChild(square);
     }   
 }
 
@@ -47,6 +46,6 @@ gridSizeButton.addEventListener('click', () => {
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', resetGrid); 
 
-showGrid(64);
+showGrid(22);
 paintGrid();
 
